a3e10d95244af55135d2b3053e2bd8dfabcbb5a6
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-03-01 13:42:51 +0100

    feat: aggiunta in pagina degli elementi dell'array

--- js/script.js
+++ js/script.js
@@ -16,11 +16,24 @@ const listaSpesa = [
   "marmellata",
   "biscotti",
   "cioccolato",
+  "detersivo",
+  "carta igienica",
+  "mandorle",
 ];
 
 let counter = 0;
 
+const tBodyEl = document.querySelector("tbody");
+console.log(tBodyEl);
+
 while (counter < listaSpesa.length) {
+  let tREl = document.createElement("tr");
+  tBodyEl.append(tREl);
+  let tHEl = document.createElement("th");
+  tHEl.scope = "row";
+  tHEl.classList.add("px-4", "fw-medium", "fs-3");
+  tHEl.innerHTML = `${counter + 1}. ${listaSpesa[counter]}`;
+  tREl.append(tHEl);
   console.log(listaSpesa[counter]);
   counter++;
 }
