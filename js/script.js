3aaa95df22ea3d8546b68e165271b98d6ce9a43e
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-03-01 13:17:59 +0100

    feat: aggiunta bozza iniziale esercizio svolto

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const listaSpesa = [
+  "pane",
+  "latte",
+  "uova",
+  "formaggio",
+  "frutta",
+  "verdura",
+  "riso",
+  "pasta",
+  "tonno",
+  "fagioli",
+  "carne",
+  "pesce",
+  "yogurt",
+  "burro",
+  "marmellata",
+  "biscotti",
+  "cioccolato",
+];
+
+let counter = 0;
+
+while (counter < listaSpesa.length) {
+  console.log(listaSpesa[counter]);
+  counter++;
+}
